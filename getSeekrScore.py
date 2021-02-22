fef1942efc0520dc5cbf482fa2b9de561c6d50c9
wdlv <woodenleaves@gmail.com>
2021-02-22 17:08:56 -0500

    fix minor errors

--- getSeekrScore.py
+++ getSeekrScore.py
@@ -1,6 +1,6 @@
 import argparse
 from seekr.fasta_reader import Reader
-import corefunctions
+#import corefunctions
 from scipy import stats
 from itertools import product
 import numpy as np
@@ -13,19 +13,18 @@ def getSeqsKmerProcessedCounts(seqs, k, alphabet):
 
     # create a numpy array with initial value 0; 4 is equal to len(ATCG)
     kmerDataMatrix = np.zeros((len(seqs), 4**k), dtype=np.float32)
+
     for index, seq in enumerate(seqs):
         # create a dict with all kmers and initial value is 0
         kmerDict = dict.fromkeys(demoKmers, 0)
-        # remove N and make sure sequences only have AGCT
-        seq=seq.replace("N","")
-        seq_set = sorted(list(set(seq + "ATCG")))
-        assert seq_set == ['A', 'C', 'G', 'T'], 'input sequences have bases not belong to ACGTN'
 
         # scale kmer count number to counts/kb of current sequence
         seq_length = len(seq)
         scaled_increment = 1000 / (seq_length - k + 1)
         for i in range(0,seq_length-k+1):
-            kmerDict[seq[i:i+k]] += scaled_increment
+            currentKmer = seq[i:i+k]
+            if currentKmer in kmerDict:
+                kmerDict[currentKmer] += scaled_increment
         
         # add scaled 1 to kmer whose count is 0
         onerow = list(kmerDict.values())
@@ -38,11 +37,11 @@ def getSeqsKmerProcessedCounts(seqs, k, alphabet):
 
     return kmerDataMatrix
 
-# get a pearson correlation score of zscores calcuated from query and interesting fastas
-def getPearsonCorrelation(oneSeq, queryMean, queryStd, interestingMean, interestingStd):
-    queryZscore = (oneSeq - queryMean) / queryStd
-    interestingZscore = (oneSeq - interestingMean) / interestingStd
-    oneSeqPearCorr = stats.pearsonr(queryZscore, interestingZscore)[1]
+# get a pearson correlation score of zscores calcuated from query and background fastas
+def getPearsonCorrelation(oneSeq, querySeq, backgroundMean, backgroundStd):
+    queryZscore = (querySeq - backgroundMean) / backgroundStd
+    backgroundZscore = (oneSeq - backgroundMean) / backgroundStd
+    oneSeqPearCorr = stats.pearsonr(queryZscore, backgroundZscore)[1]
 
     return oneSeqPearCorr
 
@@ -52,7 +51,7 @@ def getPearsonCorrelation(oneSeq, queryMean, queryStd, interestingMean, interest
 parser = argparse.ArgumentParser()
 parser.add_argument('--queryFasta', type=str,help='Path to query fasta file', required=True)
 #parser.add_argument('--nullFasta', type=str,help='Path to null fasta file', required=True)
-parser.add_argument('--interestingFasta', type=str,help='Path to interesting fasta file', required=True)
+parser.add_argument('--backgroundFasta', type=str,help='Path to lncRNA background sequences fasta file; used to calculate mean and standard deviation for each k-mer', required=True)
 parser.add_argument('--mSEEKRdataframeDir',type=str,help='directory to read in mSEEKR dataframe',default='./', required=True)
 parser.add_argument('--name',type=str,help='name for seekPearsonCorr file',default='out', required=True)
 parser.add_argument('--dir',type=str,help='directory to save output',default='./')
@@ -62,31 +61,30 @@ parser.add_argument('-a',type=str,help='String, Alphabet to generate k-mers (e.g
 args = parser.parse_args()
 
 
-
 if __name__ == '__main__':
 
     # Read in specified values of k, and the alphabet
     kVals = int(args.k)
     alphabet = args.a.upper()
 
-    #SEEKR fasta reader module
+    # SEEKR fasta reader module
     # get mean and std; axis = 0 -> column | axis = 1 -> row
 
-    # query fasta
+    # query fasta. Assume query fasta is one sequence. If not, merge multiple sequences to one sequence
     F = Reader(args.queryFasta)
     seqs = F.get_seqs()
-    kmerDataMatrix = getSeqsKmerProcessedCounts(seqs, kVals, alphabet)
-    queryMean = np.mean(kmerDataMatrix, axis = 0)
-    queryStd = np.std(kmerDataMatrix, axis = 0)
 
-    # interesting fasta
-    F = Reader(args.interestingFasta)
+    if len(seqs) > 1:
+        seqs = ''.join(seqs)
+        querySeq = getSeqsKmerProcessedCounts([seqs], kVals, alphabet)[0]
+    querySeq = getSeqsKmerProcessedCounts(seqs, kVals, alphabet)[0]
+
+    # background fasta
+    F = Reader(args.backgroundFasta)
     seqs = F.get_seqs()
     kmerDataMatrix = getSeqsKmerProcessedCounts(seqs, kVals, alphabet)
-    interestingMean = np.mean(kmerDataMatrix, axis = 0)
-    interestingStd = np.std(kmerDataMatrix, axis = 0)
-
-    #print(queryMean, queryStd, interestingMean, interestingStd)
+    backgroundMean = np.mean(kmerDataMatrix, axis = 0)
+    backgroundStd = np.std(kmerDataMatrix, axis = 0)
 
     # read in mSEEKR output dataframe
     mseekrdf = pd.read_csv(args.mSEEKRdataframeDir, sep="\t")
@@ -96,19 +94,11 @@ if __name__ == '__main__':
     pearsonlist = []
     # iterate each hits and get each's pearson correlation score
     for singleSeq in hitsSeqs:
-        oneSeq = getSeqsKmerProcessedCounts(singleSeq, kVals, alphabet)
-        print(singleSeq)
-        print(oneSeq)
-        oneSeq = oneSeq[0]
-        break
-'''
-        pearsonlist.append(getPearsonCorrelation(oneSeq, queryMean, queryStd, interestingMean, interestingStd))
+        oneSeq = getSeqsKmerProcessedCounts([singleSeq], kVals, alphabet)[0]
+        pearsonlist.append(getPearsonCorrelation(oneSeq, querySeq, backgroundMean, backgroundStd))
     
     # add pearsonC list to mSEEKR dataframe
     mseekrdf['seekrPearsonCorr'] = pearsonlist
 
     mseekrdf.to_csv(f'./{args.name}_{kVals}_seekr_pearson_score.txt',sep='\t', index=False)
-
-'''
-
 
